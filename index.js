dfe8351aa26d746a379a55aa4496da3e85725605
witold1885 <wiktor8555@gmail.com>
2022-09-08 11:24:35 +0300

    Webhook

--- index.js
+++ index.js
@@ -9,12 +9,26 @@ app.get('/', (req, res) => {
 });
  
 app.get('/webhook', (req, res) => {
+  /*if ($_GET['hub_mode'] == 'subscribe' && $_GET['hub_verify_token'] == 'ROwNceprOpAINgRa') {
+            return $_GET['hub_challenge'];
+        }*/
+  console.log(req.query)
   res
     .status(200)
     .send('ROwNceprOpAINgRa')
     .end();
 });
- 
+  /*form_data = request.query_params
+    mode = form_data.get('hub.mode')
+    token = form_data.get('hub.verify_token')
+    challenge = form_data.get('hub.challenge')
+    if mode and token:
+        if mode == 'subscribe' and token == "mytestingtoken":
+            print("WEBHOOK_VERIFIED")
+            return JsonResponse({"code":200,'message':challenge})
+        else:
+            return JsonResponse({"code":403})
+    return JsonResponse({"code":200,'message':'test'})*/
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
